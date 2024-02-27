5a6c8ed2bbb4a1611a883790f0cef9b90a675d92
Yukishow <g0919397742@gmail.com>
2024-02-27 23:27:53 +0800

    code framework rewrite

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#修改框架 這樣要修改比較好code也比較整齊
 from ultralytics import YOLO
 from PyQt5 import QtWidgets, QtGui
 from PIL import Image
@@ -6,69 +7,66 @@ import shutil
 import sys
 import os
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setObjectName("MainWindow")
-        self.setWindowTitle('KURO SHIO PHOTO-ID')  # 設定標題
-        self.resize(900, 960)
-        self.ui()
+#檔案開啟
+def openfile():
+    global filePath
+    filePath, _ = QtWidgets.QFileDialog.getOpenFileName(filter='IMAGE(*.jpg *.png *.JPG)') #限制檔案.jpg .png .JPG
+    if filePath:
+        img = QtGui.QPixmap(filePath)
+        img = img.scaled(900, 480)
+        openscene.addPixmap(img)
+        opengrview.setScene(openscene)
+        # print(filePath)
+    return
 
-    def ui(self):
-        btn1 = QtWidgets.QPushButton(self)
-        btn1.move(20, 20)
-        btn1.setText('選擇圖片')
-        btn1.clicked.connect(self.openfile)
-        btn2 = QtWidgets.QPushButton(self)
-        btn2.move(20, 50)
-        btn2.setText('執行YOLO')
-        btn2.clicked.connect(self.yolomodel)
-        #右上圖
-        self.grview1 = QtWidgets.QGraphicsView(self)
-        self.grview1.setGeometry(400, 0, 500, 480)
-        self.scene1 = QtWidgets.QGraphicsScene()
-        self.scene1.setSceneRect(0, 0, 900, 480)
-        self.grview1.setScene(self.scene1)
-        #右下圖
-        self.grview2 = QtWidgets.QGraphicsView(self)
-        self.grview2.setGeometry(400, 480, 500, 480)
-        self.scene2 = QtWidgets.QGraphicsScene()
-        self.scene2.setSceneRect(0, 0, 900, 480)
-        self.grview2.setScene(self.scene2)
+#Yolo model(這邊應該還要再修)
+def yolomodel():
+    model = YOLO("YOLO/best.pt")
+    result = model.predict(source=filePath,mode="predict",project="temp",name="predict",save=True,imgsz=(512, 512),conf=0.51,device = "cpu")
+    # print(result)
+    #一次一張，垃圾桶會出問題
+    filename = (os.listdir('temp/predict'))[0]
+    if filename.endswith(('.png', '.jpg', '.JPG')):
+        file = os.path.join('temp/predict', filename)
+        img = QtGui.QPixmap(file)
+        img = img.scaled(900, 480)
+        modelscene.clear()
+        modelscene.addPixmap(img)
+        modelgrview.setScene(modelscene)
+        #檔案搬移
+        shutil.move(file,'test') #修改寫法
+        shutil.rmtree(Path('temp'))
 
-    def openfile(self):
-        global filePath
-        filePath, _ = QtWidgets.QFileDialog.getOpenFileName()
-        if filePath:
-            img = QtGui.QPixmap(filePath)
-            img = img.scaled(900, 480)
-            self.scene1.clear()
-            self.scene1.addPixmap(img)
-            self.grview1.setScene(self.scene1)
-            # print(filePath)
-    
-    def yolomodel(self):
-        self.model = YOLO("YOLO/best.pt")
-        result = self.model.predict(source=filePath,mode="predict",project="temp",name="predict",save=True,imgsz=(512, 512),conf=0.51,device = "cpu")
-        # print(result)
-        #一次一張
-        #垃圾桶會出問題
-        filename = (os.listdir('temp/predict'))[0]
-        if filename.endswith(('.png', '.jpg', '.jpeg', '.JPG')):
-            file = os.path.join('temp/predict', filename)
-            img = QtGui.QPixmap(file)
-            img = img.scaled(900, 480)
-            self.scene2.clear()
-            self.scene2.addPixmap(img)
-            self.grview2.setScene(self.scene2)
-            #檔案搬移
-            shutil.move(os.path.join('temp/predict', filename), 'test')
-            tempfile = Path('temp')
-            shutil.rmtree(tempfile)
+#介面框架
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+MainWindow.setObjectName("MainWindow")
+MainWindow.setWindowTitle("KURO SHIO PHOTO-ID") # 設定標題
+MainWindow.resize(900, 960) #視窗大小應該還需要調整
 
+openbtn = QtWidgets.QPushButton(MainWindow)
+openbtn.move(20, 20)
+openbtn.setText('選擇圖片')
+openbtn.clicked.connect(openfile)
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    Form = MyWidget()
-    Form.show()
-    sys.exit(app.exec_())
+modelbtn = QtWidgets.QPushButton(MainWindow)
+modelbtn.move(20, 50)
+modelbtn.setText('執行YOLO')
+modelbtn.clicked.connect(yolomodel)
+
+#右上圖(原圖應該不需要)
+opengrview = QtWidgets.QGraphicsView(MainWindow)
+opengrview.setGeometry(400, 0, 500, 480)
+openscene = QtWidgets.QGraphicsScene()
+openscene.setSceneRect(0, 0, 900, 480)
+opengrview.setScene(openscene)
+#右下圖(Yolo的結果圖)
+modelgrview = QtWidgets.QGraphicsView(MainWindow)
+modelgrview.setGeometry(400, 480, 500, 480)
+modelscene = QtWidgets.QGraphicsScene()
+modelscene.setSceneRect(0, 0, 900, 480)
+modelgrview.setScene(modelscene)
+#存法目前不知道
+
+MainWindow.show()
+sys.exit(app.exec_())
