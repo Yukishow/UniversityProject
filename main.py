88912f6f53e5cb762e791dd6b153e7f5a10dc296
Yukishow <g0919397742@gmail.com>
2024-03-10 23:50:27 +0800

    updata save function

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#修改框架 這樣要修改比較好code也比較整齊
 from ultralytics import YOLO
 from PyQt5 import QtWidgets, QtGui
 from PIL import Image
@@ -21,8 +20,9 @@ def openfile():
 
 #Yolo model(這邊應該還要再修)
 def yolomodel():
+    global filename
     model = YOLO("YOLO/best.pt")
-    result = model.predict(source=filePath,mode="predict",project="temp",name="predict",save=True,imgsz=(512, 512),conf=0.51,device = "cpu")
+    model.predict(source=filePath,mode="predict",project="temp",name='predict',save=True,imgsz=(512, 512),conf=0.51,device = "cpu")
     # print(result)
     #一次一張，垃圾桶會出問題
     filename = (os.listdir('temp/predict'))[0]
@@ -37,12 +37,21 @@ def yolomodel():
         shutil.move(file,'test') #修改寫法
         shutil.rmtree(Path('temp'))
 
+def savefile():
+    filePath, _= QtWidgets.QFileDialog.getSaveFileName(filter='IMAGE(*.jpg *.png *.JPG)') #限制檔案.jpg .png .JPG
+    if filePath:
+        img = os.path.join('test', filename)
+        shutil.move(img,filePath)
+        # shutil.rmtree(Path('temp'))
+        
+
 #介面框架
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 MainWindow.setObjectName("MainWindow")
 MainWindow.setWindowTitle("KURO SHIO PHOTO-ID") # 設定標題
-MainWindow.resize(900, 960) #視窗大小應該還需要調整
+MainWindow.resize(1000, 800) #視窗大小應該還需要調整
+#MainWindow.setStyleSheet('background:#fcc;') #背景顏色(不是很懂反正改了很醜?)
 
 openbtn = QtWidgets.QPushButton(MainWindow)
 openbtn.move(20, 20)
@@ -54,19 +63,24 @@ modelbtn.move(20, 50)
 modelbtn.setText('執行YOLO')
 modelbtn.clicked.connect(yolomodel)
 
+savelbtn = QtWidgets.QPushButton(MainWindow)
+savelbtn.move(20, 80)
+savelbtn.setText('儲存檔案')
+savelbtn.clicked.connect(savefile)
+
 #右上圖(原圖應該不需要)
 opengrview = QtWidgets.QGraphicsView(MainWindow)
-opengrview.setGeometry(400, 0, 500, 480)
+opengrview.setGeometry(500, 0, 500, 400)
 openscene = QtWidgets.QGraphicsScene()
 openscene.setSceneRect(0, 0, 900, 480)
 opengrview.setScene(openscene)
 #右下圖(Yolo的結果圖)
 modelgrview = QtWidgets.QGraphicsView(MainWindow)
-modelgrview.setGeometry(400, 480, 500, 480)
+modelgrview.setGeometry(500, 400, 500, 400)
 modelscene = QtWidgets.QGraphicsScene()
 modelscene.setSceneRect(0, 0, 900, 480)
 modelgrview.setScene(modelscene)
-#存法目前不知道
+
 
 MainWindow.show()
 sys.exit(app.exec_())
