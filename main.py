387a1b257f9424152039d995ff8a4dc5377dc0c6
Yukishow <g0919397742@gmail.com>
2024-04-09 20:29:22 +0800

    test status_label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from image_viewer import ImageViewer
 import sys
 
@@ -21,6 +21,9 @@ class mainWindow(QtWidgets.QWidget):
         self.run_button = QtWidgets.QPushButton('執行YOLO')
         self.run_button.clicked.connect(self.runYOLO)
 
+        # self.status_label = QtWidgets.QLabel(self)
+        # self.status_label.setAlignment(QtCore.Qt.AlignCenter)
+
         self.image_button = QtWidgets.QPushButton('查看照片')
         self.image_button.clicked.connect(self.runViewer)
 
@@ -32,12 +35,14 @@ class mainWindow(QtWidgets.QWidget):
         layout.addLayout(folder_layout)
         layout.addWidget(self.image_button)
         layout.addWidget(self.run_button)
+        # layout.addWidget(self.status_label)
         self.setLayout(layout)
 
     def openFolder(self):
         folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, "選擇資料夾")
         if folder_path:
             self.folder_edit.setText(folder_path)
+            # self.status_label.setText("Not executed yet")
 
     # YOLO
     def runYOLO(self):
@@ -45,6 +50,7 @@ class mainWindow(QtWidgets.QWidget):
         if folder_path:
             model = YOLO("YOLO/best.pt")
             model.predict(source=folder_path, mode="predict", project="temp", name='predict', save=True, imgsz=(512, 512), conf=0.51, device="cpu", save_crop=True)
+            # self.status_label.setText("Finished")
             self.viewer = ImageViewer()
             folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, "選擇資料夾")
             self.viewer.loadImagePaths(folder_path)
